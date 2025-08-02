e61d78a22d465741fa05f7328bc72fc438e781c2
Sameehan Sahasrabudhe <sahasrabudhesameehan@gmail.com>
2025-08-02 19:48:12 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,9 +1,12 @@
-# This is a begining.
-
-print("Calc")
-
+# Imports
 from tkinter import *
 
+# Creating GUI
 window = Tk()
+window.geometry("500x500")
 
-a = Entry(window, width = "200")
+# Adding Inputs
+a = Entry(window, width = 56, borderwidth = 5)
+a.place(x=0, y=0)
+
+
