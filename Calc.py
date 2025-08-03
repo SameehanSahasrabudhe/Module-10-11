4c6f63bb5c56f92770b4c5b5ccdbe580027b87e8
Sameehan Sahasrabudhe <sahasrabudhesameehan@gmail.com>
2025-08-03 09:38:44 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -9,4 +9,9 @@ window.geometry("500x500")
 a = Entry(window, width = 56, borderwidth = 5)
 a.place(x=0, y=0)
 
+# Adding buttons
+b = Button(window, text="Button 1", padx=20, pady=10)
+b.place(x=0, y=40)
 
+c = Button(window, text="Button 2", padx=20, pady=10)
+c.place(x=100, y=40)
